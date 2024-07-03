ab455e7df2fd46f181e27dd7699e55adf4d8abcd
Mathias Waibel <dev.mathwaibel@gmail.com>
2024-07-03 10:47:37 -0300

    Updt 1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Imc_calc from './components/IMC_Calc/Imc_calc';
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='container'>
+
+      <Imc_calc />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/IMC_Calc/Imc_calc.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import Button from '../Button/Button';
+import './Imc_calc.css';
+
+const Imc_calc = () => {
+
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+
+  const clearForm = (e) => {
+    e.preventDefault();
+    setHeight("");
+    setWeight("");
+  };
+
+  return (
+    <div id='container'>
+      <h1 id='tittle'>Calculadora de IMC</h1>
+
+      <form id="imc_form">
+        <div className="form_inputs">
+          <div className="form_control">
+            <label htmlFor="height">Altura: </label>
+            <input 
+              type="number" 
+              name='height' 
+              id='height' 
+              placeholder='Exemplo 1,75' 
+              onChange={(e) => setHeight(e.target.value)} 
+              value={height}
+              required
+            />
+          </div>
+
+          <div className="form_control">
+            <label htmlFor="weight">Peso: </label>
+            <input 
+              type="number" 
+              name='weight' 
+              id='weight' 
+              placeholder='Exemplo 58,3'
+              onChange={(e) => setWeight(e.target.value)} 
+              value={weight}
+              required
+            />
+          </div>
+        </div>
+
+        <div className="action_control">
+          <Button id = "calc_btn" text = "Calcular"/>
+          <Button id = "clear_btn" text ="Limpar" action={clearForm}/>
+        </div>
+      </form>
+
+    </div>
+  )
+}
+
+export default Imc_calc
